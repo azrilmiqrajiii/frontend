267db73fdf1c395be4a6fc8ace0552d536dae95a
azrilmiqrajiii <azrilmiqraji.210602005@student.hamzanwadi.ac.id>
2026-01-13 13:52:36 +0800

    ubah axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
-  timeout: 10000
+  timeout: 10000,
 });
 
-// Global error handler
-axios.interceptors.response.use(
-  res => res,
-  err => {
-    if (
-      err.response?.status === 401 &&
-      window.location.pathname !== "/login"
-    ) {
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401 && window.location.pathname !== "/login") {
       console.warn("Unauthorized – session expired");
     }
     return Promise.reject(err);
   }
 );
-
 
 export default axiosInstance;
