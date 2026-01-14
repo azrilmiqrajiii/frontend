2bc9b053c3b0c0ecc3c75f3eae5ee605d346ed2b
azrilmiqrajiii <azrilmiqraji.210602005@student.hamzanwadi.ac.id>
2026-01-14 14:58:10 +0800

    ubah authLayouts.jsx

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -2,57 +2,24 @@ import logo from "/images/logo.png";
 
 const AuthLayouts = ({ children, title }) => {
   return (
-    <div
-      className="
-       min-h-screen w-full
-  flex items-center justify-center
-  bg-linear-to-br
-  from-[#0A2540]
-  via-[#123B6D]
-  to-[#1E4F91]
-  relative
-  overflow-hidden
-    "
-    >
-      <div
-        className="
-        w-full
-  max-w-sm sm:max-w-md lg:max-w-lg
-  bg-white
-  rounded-2xl
-  shadow-xl
-  p-6 sm:p-8 lg:p-10
-  border border-[#E6F1F8]
-      "
-      >
-        {/* LOGO */}
+    <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-[#0A2540] via-[#123B6D] to-[#1E4F91] px-4">
+      <div className="w-full max-w-sm sm:max-w-md lg:max-w-lg bg-white rounded-2xl shadow-xl p-6 sm:p-8 lg:p-10">
         <div className="flex justify-center mb-4">
           <img
             src={logo}
             alt="Poltekpar Lombok"
-            className="h-14 sm:h-16 object-contain "
+            className="h-12 sm:h-16 object-contain"
           />
         </div>
 
-        {/* Title */}
-        <h1
-          className="
-           text-center
-  text-2xl sm:text-3xl
-  font-bold
-  text-[#0A2540]
-  mb-1
-        "
-        >
+        <h1 className="text-center text-2xl sm:text-3xl font-bold text-[#0A2540] mb-1">
           {title}
         </h1>
 
-        {/* Subtitle */}
         <p className="text-center text-slate-500 text-sm sm:text-base mb-6">
           Sistem Penjaminan Mutu – Poltekpar Lombok
         </p>
 
-        {/* Form */}
         <div className="w-full">{children}</div>
       </div>
     </div>
